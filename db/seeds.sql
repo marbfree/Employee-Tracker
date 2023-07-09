2b8181c6e93d4cb71a1d203464ef03226f4d11a5
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-09 13:25:47 -0400

    troubleshooting solved, tables in place

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,7 +10,7 @@ VALUES ("Lead", 40, 2),
        ("Manager", 50, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tasha", "Brown", 4),
-       ("Tami", "Tan", 2, 3),
+VALUES ("Tasha", "Brown", 4, null),
        ("John", "Doe", 1, 1),
-       ("Rachel", "Cook", 3, 1);
+       ("Rachel", "Cook", 3, 1),
+       ("Tami", "Tan", 2, 3);
