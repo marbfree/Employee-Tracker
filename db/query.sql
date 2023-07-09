26ba791fa58f75262608c3e4a785d521147eb561
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-09 15:38:29 -0400

    added psuedocode and some queries

---
+++ db/query.sql
@@ -0,0 +1,14 @@
+-- shows department table
+SELECT * FROM department
+-- shows role table
+SELECT * FROM role
+-- shows combined department, employees, and role tables
+SELECT * FROM employees JOIN role ON employees.role_id = role.id JOIN department ON
+department.id = role.department_id;
+
+-- how to add department?  Prompts: enter name of the department
+-- how to add a role? Prompts: enter name, salary, and department
+-- how to add an employee? Prompts: first name, last name, role, manager
+-- how to update an employee role? prompts: select an employee and new role
+
+
--- prompts.js
+++
@@ -1,9 +0,0 @@
-function viewDept() {
-    let query = 'SELECT name FROM department'
-    connection.query(query, function(err, res) {
-        if (err) throw err;
-        console.table(res);
-    });
-};
-
-modeule.exports = prompts
