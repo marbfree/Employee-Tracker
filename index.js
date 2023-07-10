e554d5cf6fba2f13d03997b8e53ab554d5c43673
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-10 15:00:07 -0400

    troubleshooting inserting multiple values

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2');
 // prompts for action on database
 const questions = [
     {
-        type: 'list', 
+        type: 'list',
         name: 'Select',
         message: 'What would you like to do?',
         choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add a Role', 'Add an Employee', 'Update An Employee Role']
@@ -16,9 +16,9 @@ const questions = [
 
 const db = mysql.createConnection(
     {
-        host: 'localhost', 
-        user: 'root', 
-        password: '', 
+        host: 'localhost',
+        user: 'root',
+        password: '',
         database: 'employee_db'
     },
     console.log('Connected to the employee_db database.')
@@ -27,44 +27,122 @@ const db = mysql.createConnection(
 // initiates the prompts then shows table for each prompt
 function init() {
     inquirer.prompt(questions)
-    .then((answers) => {
-        // create table for answer
-        if(answers.Select === "View All Departments") {
-            viewDept();
-            return;
-        }
-        if(answers.Select === "Add A Department") {}
-            addDept();
-            return;
-    })
+        .then((answers) => {
+            // create table for answer
+            if (answers.Select === "View All Departments") {
+                viewDept();
+                return;
+            }
+            if (answers.Select === "View All Roles") {
+                viewRoles();
+                return;
+            }
+            if (answers.Select === "View All Employees") {
+                viewEmployee();
+                return;
+            }
+            if (answers.Select === "Add A Department") {
+                addDept();
+                return;
+            }
+            if (answers.Select === "Add a Role") {
+                addRole();
+                return;
+            }
+        })
 }
 
 
 function viewDept() {
     let query = "SELECT * FROM department"
-    db.query(query, function(err, res) {
+    db.query(query, function (err, res) {
         if (err) throw err;
         console.table(res);
         init();
-    })    
+    })
+}
+
+function viewRoles() {
+    let query = "SELECT * FROM role"
+    db.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        init();
+    })
+}
+
+function viewEmployee() {
+    let query = "SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id"
+    db.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        init();
+    })
 }
 
 function addDept() {
     inquirer.prompt([
-    { type: 'input', 
-      name: 'department',
-      message: 'Enter the name of the new department'
-    }
+        {
+            type: 'input',
+            name: 'department',
+            message: 'Enter the name of the new department.'
+        }
     ])
-    .then((answers) => {
-        let query = `INSERT INTO department (name)
+        .then((answers) => {
+            let query = `INSERT INTO department (name)
         VALUES ("${answers.department}")`
-        db.query(query, function(err, res) {
-            if (err) throw err;
-            console.log('Success!');
-            init();
-        })    
-    })   
+            db.query(query, function (err, res) {
+                if (err) throw err;
+                console.log('Success!');
+                init();
+            })
+        })
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message:"Enter the title of the new role."
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: "Enter the salary for the new role."
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: "Enter the department id."
+        }
+    ])
+        .then((answers) => {
+            let query = `INSERT INTO role (title, salary, department_id) VALUES ("${answers.title, answers.salary, answers.department_id}")`
+            db.query(query, function (err, res) {
+                if (err) throw err;
+                console.log('Success!');
+            
+            // let query2 = `INSERT INTO role (salary) VALUES ("${answers.salary}")`
+            // db.query(query2, function (err, res) {
+            //     if (err) throw err;
+            //     console.log('Success!');
+            // })
+    })});
+//     inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'roleSalary',
+//             message: 'Enter the salary for the new role.'  
+//         }
+//     ])
+//         .then((answers) => {
+//             let query = `INSERT INTO role (salary) VALUES("${answers.roleSaoary}")`
+//             db.query(query, function (err, res) {
+//                 if (err) throw err;
+//                 console.log('Success!');
+//             })
+//         })
 }
 init();
 
